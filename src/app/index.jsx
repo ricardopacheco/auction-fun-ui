fec85c29c1226dd2a961f1e21e88578d2b8f2157
Ricardo Pacheco <499567+ricardopacheco@users.noreply.github.com>
2024-04-02 13:29:24 -0300

    Add expo router

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,10 @@
-import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Page() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Expo router!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +18,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-registerRootComponent(App);
